aa14083460d49ac1b1993f2e58dc8fd5d045cd9e
matt <mattcollie@Live.com>
2019-02-11 23:18:14 +1300

    changing to new style of website

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,29 +6,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    import datetime
+    years = datetime.date.today().year - 2015
 
-
-@app.route('/about', methods=['GET'])
-def about():
-    return render_template('about.html')
-
-
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        email = request.form.get('email', None)
-        subject = request.form.get('subject', None)
-        message = request.form.get('message', None)
-        with open("contact.txt", "a") as file:
-            file.writelines([
-                'email: {}\n'.format(email),
-                'subject: {}\n'.format(subject),
-                'message: {}\n'.format(message),
-                '\n'
-            ])
-
-    return render_template('contact.html')
+    return render_template('index.html', years=years)
 
 
 if __name__ == '__main__':
